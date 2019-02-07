ecfe98f04661d7a75f032affd4786b6378d1e292
Christopher Leidigh <cleidigh@Gmail.com>
2019-02-07 01:11:58 -0500

    Update version 0.8. 12

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,5 @@
 
 ## Versions
 0.8.10	- Last version by Philoux - Compatible with Thunderbird 17.0-31.*
-0.8.11	- Updated by cleidigh for TB60 - TB65 (legacy w manifest)
+0.8.11	- disable throwaway
+0.8.12	- Updated by cleidigh for TB60 - TB65 (legacy w manifest)
--- scripts/build-60-65-xpi.bat
+++ scripts/build-60-65-xpi.bat
@@ -1,3 +1,3 @@
 call jpm xpi --dest-dir ./xpi --addon-dir ./src
-call 7z d .\xpi\localfolder-0.8.11-tb.xpi bootstrap.js
-call 7z a .\xpi\localfolder-0.8.11-tb.xpi .\src\manifest.json
+call 7z d .\xpi\localfolder-0.8.12-tb.xpi bootstrap.js
+call 7z a .\xpi\localfolder-0.8.12-tb.xpi .\src\manifest.json
