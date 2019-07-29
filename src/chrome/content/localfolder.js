5a949bea4c0e24efa400c1d79c264149d2df92d9
Christopher Leidigh <cleidigh@Gmail.com>
2019-07-29 00:17:17 -0400

    Rough working emptyTrash, storeContractID

--- src/chrome/content/localfolder.js
+++ src/chrome/content/localfolder.js
@@ -11,20 +11,17 @@ var { Services } = ChromeUtils.import('resource://gre/modules/Services.jsm');
 
 eu.philoux.localfolder.lastFolder = "";
 
-const SEND_FOLDER_FLAGS = 0x200;
-const DRAFTS_FOLDER_FLAGS = 0x400;
-const TEMPLATES_FOLDER_FLAGS = 0x400000;
-const ARCHIVES_FOLDER_FLAGS = 0x4000;
-const JUNK_FOLDER_FLAGS = 0x40000000;
-
+eu.philoux.localfolder.pendingFolders = [];
 
 eu.philoux.localfolder.specialFolders = {
-    "Inbox": { "directoryName": "Inbox", "flags": SEND_FOLDER_FLAGS },
-    "Sent": { "directoryName": "Sent", "flags": SEND_FOLDER_FLAGS },
-    "Drafts": { "directoryName": "Drafts", "flags": DRAFTS_FOLDER_FLAGS },
-    "Templates": { "directoryName": "Templates", "flags": TEMPLATES_FOLDER_FLAGS },
-    "Archives": { "directoryName": "Archives", "flags": ARCHIVES_FOLDER_FLAGS },
-    "Junk": { "directoryName": "Junk", "flags": JUNK_FOLDER_FLAGS }
+    "Inbox": { "directoryName": "Inbox", "flags": (Ci.nsMsgFolderFlags.Mail | Ci.nsMsgFolderFlags.Inbox) },
+    "Sent": { "directoryName": "Sent", "flags": (Ci.nsMsgFolderFlags.Mail | Ci.nsMsgFolderFlags.SentMail) },
+    "Trash": { "directoryName": "Trash", "flags": (Ci.nsMsgFolderFlags.Mail | Ci.nsMsgFolderFlags.Trash) },
+    "Drafts": { "directoryName": "Drafts", "flags": (Ci.nsMsgFolderFlags.Mail | Ci.nsMsgFolderFlags.Drafts) },
+    "Templates": { "directoryName": "Templates", "flags": (Ci.nsMsgFolderFlags.Mail | Ci.nsMsgFolderFlags.Templates) },
+    "Archives": { "directoryName": "Archives", "flags": (Ci.nsMsgFolderFlags.Mail | Ci.nsMsgFolderFlags.Archive) },
+    "Junk": { "directoryName": "Junk", "flags": (Ci.nsMsgFolderFlags.Mail | Ci.nsMsgFolderFlags.Junk) },
+    "Outbox": { "directoryName": "Unsent Messages", "flags": (Ci.nsMsgFolderFlags.Mail | Ci.nsMsgFolderFlags.Queue) }
 }
 
 // Extension Information Icons
@@ -58,29 +55,36 @@ eu.philoux.localfolder.toggleSpecialFolders = function (specialFolder) {
 
     if (!!addAllCheckbox.checked && !addFolderElement.checked) {
         addAllCheckbox.checked = false;
-        console.debug('toggle wall');
     } else if (!addAllCheckbox.checked && addFolderElements.every(e => e.checked)) {
         addAllCheckbox.checked = true;
     }
 
 }
 
-    eu.philoux.localfolder.addSpecialFolders = function (aParentFolder) {
+    eu.philoux.localfolder.addSpecialFolders = function (aParentFolder, aParentFolderPath) {
     
     eu.philoux.localfolder.LocalFolderTrace("Add special folder's");
     let addFolderElements = document.querySelectorAll("[id^='add_folder_']");
-    // var filespec = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsIFile);
+
+    msgWindow = Cc["@mozilla.org/messenger/msgwindow;1"].createInstance(Ci.nsIMsgWindow);
 
     for (let index = 0; index < addFolderElements.length; index++) {
         const element = addFolderElements[index];
         if (!!element.checked) {
             // Add special folder
             const l = element.getAttribute("SpecialFolder");
-            eu.philoux.localfolder.LocalFolderTrace('A folder: ' + l);
+            // const storeID = aParentFolder.server.storeContractID;
+            const storeID =  aParentFolder.server.getCharValue("storeContractID");
+
+            eu.philoux.localfolder.LocalFolderTrace('Add special folder: ' + l + '  ' + storeID);
             if (l !== "Trash" && l !== "Unsent Messages") {
-                eu.philoux.localfolder.LocalFolderTrace('AddSubfolder folder: ' + l);
-                let sf = aParentFolder.addSubfolder(l);
-                sf.setFlag(eu.philoux.localfolder.specialFolders[l].flags)
+                eu.philoux.localfolder.LocalFolderTrace('AddSubfolder folder/fix 11: ' + l);
+                // let sf = aParentFolder.addSubfolder(l);
+                aParentFolder.createSubfolder(l, msgWindow);
+                aParentFolder.getChildNamed(l).flags = eu.philoux.localfolder.specialFolders[l].flags;
+
+                eu.philoux.localfolder.fixupSubfolder(aParentFolderPath, l, false, storeID);
+                eu.philoux.localfolder.LocalFolderTrace('AddSubfolder folder: After fix');
         
             }
             
@@ -165,8 +169,13 @@ eu.philoux.localfolder.btCreeDossierLocal = function () {
             eu.philoux.localfolder.LocalFolderAfficheMsgId("DossierPasRenseigne");
             return false;
         }
+
+        // cleidigh - handle storage type
+        var storeID = document.getElementById("server.storeTypeMenulist").value;
+        var emptyTrashOnExit = document.getElementById("server.emptyTrashOnExit").checked;
+
         //création du dossier local
-        eu.philoux.localfolder.creeDossierLocal(nom, dossier);
+        eu.philoux.localfolder.creeDossierLocal(nom, dossier, storeID, emptyTrashOnExit);
     } catch (ex) {
         eu.philoux.localfolder.LocalFolderAfficheMsgId2("ErreurCreationDossier", ex);
         window.close();
@@ -255,7 +264,7 @@ eu.philoux.localfolder.SelectChemin = function () {
  *	implémentation : l'appel à cette fonction suppose que l'appelant a vérifier que le compte n'existe pas déjà
  *	nom et chemin pas déjà utilisés
  */
-eu.philoux.localfolder.creeDossierLocal = function (nom, chemin) {
+eu.philoux.localfolder.creeDossierLocal = function (nom, chemin, storeID, emptyTrashOnExit) {
 
     try {
         var accountmanager = Components.classes["@mozilla.org/messenger/account-manager;1"].getService(Components.interfaces.nsIMsgAccountManager);
@@ -264,19 +273,71 @@ eu.philoux.localfolder.creeDossierLocal = function (nom, chemin) {
         filespec.initWithPath(chemin);
         srv.prettyName = nom;
         srv.localPath = filespec;
-        eu.philoux.localfolder.LocalFolderTrace("CreateLocal  folder: "+ chemin);
+
+        let defaultStoreID = Services.prefs.getCharPref("mail.serverDefaultStoreContractID");
+        srv.setCharValue("storeContractID", storeID);
+        srv.emptyTrashOnExit = emptyTrashOnExit;
+        
+        // let defaultStoreID = Services.prefs.getCharPref("mail.serverDefaultStoreContractID");
+        let c = srv.getBoolValue("canChangeStoreType");
+        
+        eu.philoux.localfolder.LocalFolderTrace("CreateLocal 3  folder: "+ chemin + "\neTrash : " + emptyTrashOnExit);
+        eu.philoux.localfolder.LocalFolderTrace("defaultstore: " + defaultStoreID);
+        // srv.storeContractId = "@mozilla.org/msgstore/maildirstore;1";
+
+        eu.philoux.localfolder.LocalFolderTrace("CanChangestore: " + c);
+        eu.philoux.localfolder.LocalFolderTrace("Original store/Current: "+ storeID + "  " );
+        
         eu.philoux.localfolder.lastFolder = chemin;
-        eu.philoux.localfolder.addSpecialFolders(srv.rootFolder);
+        
+        // Services.prompt.alert(window, "progress", "Before at subfolders");
 
         var account = accountmanager.createAccount();
         account.incomingServer = srv;
 
-        // srv.rootFolder.deleteSubFolders(["Trash"], null);
-        // eu.philoux.localfolder.LocalFolderTrace('folder method '+ folderpane.p1);
-        // eu.philoux.localfolder.LocalFolderTrace('folder trash ');
-        // folderpane.refresh();
+        // Services.prompt.alert(window, "progress", "After create account");
 
-        eu.philoux.localfolder.LocalFolderTrace('folder names ' + chemin + '   ' + eu.philoux.localfolder.lastFolder);
+		eu.philoux.localfolder.fixupSubfolder(chemin, "Trash", false, storeID);
+		eu.philoux.localfolder.fixupSubfolder(chemin, "Unsent Messages", false, storeID);
+		
+		// Services.prompt.alert(window, "progress", "After Trash, Unsent");
+/* 		
+		msgWindow = Cc["@mozilla.org/messenger/msgwindow;1"].createInstance(Ci.n sIMsgWindow);
+
+		srv.rootMsgFolder.createSubfolder("Drafts", msgWindow);
+		folderChild = srv.rootMsgFolder.getChildNamed("Drafts");
+		eu.philoux.localfolder.LocalFolderTrace("created subfolder: "+ folderChild.name );
+
+		folderChild.flags = (Ci.nsMsgFolderFlags.Mail | Ci.nsMsgFolderFlags.Drafts);
+		eu.philoux.localfolder.LocalFolderTrace("set flags on subfolder: " );
+		
+		eu.philoux.localfolder.fixupSubfolder(chemin, "Drafts", false);
+
+
+
+		srv.rootMsgFolder.createSubfolder("Junk", msgWindow);
+		folderChild = srv.rootMsgFolder.getChildNamed("Junk");
+		eu.philoux.localfolder.LocalFolderTrace("created subfolder: "+ folderChild.name );
+
+		folderChild.flags = (Ci.nsMsgFolderFlags.Mail | Ci.nsMsgFolderFlags.Junk);
+		eu.philoux.localfolder.LocalFolderTrace("set flags on subfolder: Junk" );
+		
+		eu.philoux.localfolder.fixupSubfolder(chemin, "Junk", false);
+
+
+ */
+		eu.philoux.localfolder.LocalFolderTrace("Created Drafts, msf, empty Drafts, set special flags" );
+
+		eu.philoux.localfolder.pendingFolders.push(chemin);
+
+        eu.philoux.localfolder.addSpecialFolders(srv.rootMsgFolder, chemin);
+        eu.philoux.localfolder.LocalFolderTrace("had special subfolder:" );
+
+		var notifyFlags = Ci.nsIFolderListener.added;
+
+		srv.rootMsgFolder.AddFolderListener(FolderListener, notifyFlags);
+		eu.philoux.localfolder.LocalFolderTrace("Added folder listener");
+
 
         return account;
     } catch (ex) {
@@ -286,6 +347,73 @@ eu.philoux.localfolder.creeDossierLocal = function (nom, chemin) {
 
     return false;
 }
+
+
+eu.philoux.localfolder.fixupSubfolder = function (parentName, folderName, removeFileFolder, storeID) {
+
+	eu.philoux.localfolder.LocalFolderTrace(`fixupSubfolder: ${folderName} - remove file folder: ${removeFileFolder}  storeType: ${storeID}`);
+	var filespec = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsIFile);
+    var rf = `${parentName}\\${folderName}`
+    
+    // let StoreID = srv.getCharValue("storeContractID");
+
+    // var StoreID = "@mozilla.org/msgstore/maildirstore;1";
+	filespec.initWithPath(parentName);
+	filespec.append(folderName);
+
+    eu.philoux.localfolder.LocalFolderTrace(`saving folder: ${rf}`);
+	// if (removeFileFolder) {
+    if (storeID !== "@mozilla.org/msgstore/maildirstore;1") {
+        eu.philoux.localfolder.LocalFolderTrace(`removing file folder: ${rf}`);
+        try {
+    		filespec.remove(true);
+            
+        } catch (error) {
+            eu.philoux.localfolder.LocalFolderTrace(`no folder found removing file folder: ${rf}`);
+        }
+    }
+    
+    if (storeID === "@mozilla.org/msgstore/maildirstore;1") {
+        filespec.create(Components.interfaces.nsIFile.DIRECTORY_TYPE, 0755);
+    	eu.philoux.localfolder.LocalFolderTrace(`fixupSubfolder done - CREATED DIRECTORY`);
+
+    } else {
+        filespec.create(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0644);
+        eu.philoux.localfolder.LocalFolderTrace(`fixupSubfolder done - create file`);
+    }
+    }
+
+var FolderListener = {
+	OnItemAdded: function(parentFolder, aItem)
+     { 
+		eu.philoux.localfolder.LocalFolderTrace(`item added 3: ${parentFolder.filePath.path} ${parentFolder.flags}  ${aItem.filePath.path}  ${aItem.flags} ${eu.philoux.localfolder.pendingFolders[0]}`);
+		var rf =  `${aItem.filePath.path}`;
+		rf = rf.replace(`\\${aItem.name}`, "");
+ 
+		if ( eu.philoux.localfolder.pendingFolders.includes(`${rf}`) && (aItem.flags & 0x000004)) {
+			var filespec = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsIFile);
+			eu.philoux.localfolder.fixupSubfolder(eu.philoux.localfolder.pendingFolders[0], aItem.name, false, aItem.server.getCharValue("storeContractID"));
+			
+			if (aItem.name in eu.philoux.localfolder.specialFolders) {
+				var sf = eu.philoux.localfolder.specialFolders[aItem.name].flags
+				eu.philoux.localfolder.LocalFolderTrace(`${aItem.name} get flags ${aItem.flags}`);
+				aItem.setFlag(sf);
+				eu.philoux.localfolder.LocalFolderTrace(`${aItem.name} set flags (${sf} : ${aItem.flags}`);
+			}
+			
+		}
+     }, 
+
+	OnItemRemoved() {},
+	OnItemPropertyChanged() {},
+	OnItemIntPropertyChanged() {},
+	OnItemBoolPropertyChanged() {},
+	OnItemUnicharPropertyChanged() {},
+	OnItemPropertyFlagChanged() {},
+	OnItemEvent() {},
+  };
+  
+
 
 /**
  *	test si un répertoire est valide pour créer un dossier local
